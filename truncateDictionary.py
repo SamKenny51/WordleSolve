76068139e89aba56d759337cefcbffd9e9b5dd73
Sam Kenny <sgkenny@ncsu.edu>
2022-03-14 18:14:13 -0400

    Update truncateDictionary.py

--- truncateDictionary.py
+++ truncateDictionary.py
@@ -12,19 +12,6 @@ for word in data:
                 print(word, 2)
             truncDict.append(word[:-1])
 
-
-'''
-freq = collections.Counter("".join(truncDict))
-
-for i in range(len(truncDict)):
-    wordScore = 0
-    for c in truncDict[i]:
-        wordScore += freq[c]
-    truncDict[i] = (truncDict[i], wordScore)
-
-truncDict.sort(key=lambda x: x[1], reverse=True)
-
-'''
 print('######')
 for i in range(len(truncDict)):
     if truncDict[i][:3] == "spi":
